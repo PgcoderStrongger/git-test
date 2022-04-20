218dbe96334aca474f6b5157f9e328756cc3cb9e
pg <pgcoder@qq.com>
2022-04-20 13:05:58 +0800

    third commite by idea

--- git01/src/GitTest.java
+++ git01/src/GitTest.java
@@ -1,7 +1,7 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println("heelo git2");
+        System.out.println("hello git2");
         System.out.println("hello git3");
     }
 }
